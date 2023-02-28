0bd09b2a2e659c30359b12c7d9c75750cc34b757
LêMinhThành <68367028+minhthanh120@users.noreply.github.com>
2023-02-28 15:44:24 +0700

    fix model

---
+++ QuanLyLopHoc/Migrations/20230228084304_db02.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyLopHoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class db02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDay",
+                table: "UserClass",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "UserClass",
+                type: "NVARCHAR(20)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "UserClass",
+                type: "Char(12)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDay",
+                table: "UserClass");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "UserClass");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "UserClass");
+        }
+    }
+}
