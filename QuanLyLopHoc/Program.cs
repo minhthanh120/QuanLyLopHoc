07e801623f9ecf3492d4bdaf65aed9dd625c4b2c
VuHuyQuang278 <77908719+VuHuyQuang278@users.noreply.github.com>
2023-05-08 18:26:33 +0700

    Update list student

--- QuanLyLopHoc/Program.cs
+++ QuanLyLopHoc/Program.cs
@@ -10,6 +10,8 @@ using QuanLyLopHoc.Repositories;
 using QuanLyLopHoc.Services;
 using QuanLyLopHoc.Hubs;
 using QuanLyLopHoc.Models.DAO;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using QuanLyLopHoc.Services.FunctionSerives;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("SMContextConnection") ?? throw new InvalidOperationException("Connection string 'SMContextConnection' not found.");
@@ -20,7 +22,8 @@ builder.Services.AddDbContext<SMContext>(options =>
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<SMContext>();
-
+var mailsetting = builder.Configuration.GetSection("MailSettings");
+builder.Services.Configure<MailSettings>(mailsetting);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR(e => e.MaximumReceiveMessageSize = 102400000);
@@ -31,7 +34,34 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMessageSevice, MessageService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddSingleton<IEmailSender, SendMailService>();
 
+builder.Services.AddAuthentication().AddGitHub(options =>
+{
+    options.ClientId = "aef6fe19fe5192457d96";
+    options.ClientSecret = "b1c897493c258fdc74f7a40f619c313c88e72f08";
+    options.CallbackPath = "/signin-github";
+});
+
+builder.Services.AddAuthentication().AddFacebook(facebookOptions =>
+{
+    //IConfigurationSection FBAuthNSection = builder.Configuration.GetSection("Authentication:FB");
+    facebookOptions.AppId = "3390710921258513";
+    facebookOptions.AppSecret = "2ee20fc4a69b55e90757b4eb49c5a250";
+    facebookOptions.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    // Đọc thông tin Authentication:Google từ appsettings.json
+    //IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+
+    // Thiết lập ClientID và ClientSecret để truy cập API google
+    googleOptions.ClientId = "669118303251-adpk9vds99geasqs4ac55ja23gr3g7m1.apps.googleusercontent.com";
+    googleOptions.ClientSecret = "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";
+    // Cấu hình Url callback lại từ Google (không thiết lập thì mặc định là /signin-google)
+    googleOptions.CallbackPath = "/google-login";
+
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
